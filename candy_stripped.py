6b11282e17da01ba1441d3b3e51c63cd17bfbe31
~es <46006871+m0nkeyplay@users.noreply.github.com>
2019-02-16 20:59:47 -0600

    Updated structure for simpler code

--- candy_stripped.py
+++ candy_stripped.py
@@ -1,10 +1,19 @@
 #!/usr/bin/python3
-
-# shell of a script to talk to Candy for root-me.org Challenges.
-
-# results have been removed
-
-#  Fill in a nickname down there.... it's helpful.  I promise.
+#
+#  author:		https://github.com/m0nkeyplay
+#               Use as you wish, this is cobbled together from bits of code I saw trying to work out root-me.org
+#
+#  Feb 16, 2019
+#
+#  purpose:     Shell of a script to talk to Candy
+#               Candy is from root-me.org  She's a nice bot in the Programming section
+#
+#
+#  Answers are not provided, but I didn't take out all the imports I needed to solve !ep1-4
+#  REMOVED can be replaced with info to solve the puzzles - or the whole solve_puzzle function can be removed if you wish
+#
+#  You should
+#       ! Fill in a NICKNAME below
 
 from sys import argv
 import math
@@ -25,6 +34,12 @@ if len(argv) == 1:
     exit()
 else:
     puzzle = argv[1]
+    
+argCheck = re.search('!ep[1-4]',puzzle)
+if not argCheck:
+    print('Okay, we are working with a limited set of arguments here.  Please pass argument as \'!ep[1-4]\'')
+    exit()
+          
 
 SERVER = 'irc.root-me.org'
 PORT = 6667
@@ -45,49 +60,75 @@ def join(channel):
     send_data('JOIN %s' %channel)
     print('Joined %s' % channel)
 
-def login(nickname, username='user', password = None, realname='WhyAsking', hostname='MyHost', servername='MyServerDoesHaveAName'):
+def login(nickname, username='user', password = None, realname='WhyAsking', hostname='dsotm', servername='Server'):
     send_data('USER %s %s %s %s' %(username, hostname, servername, realname))
     send_data("NICK " + nickname)
-    print("Logged in")
+    print("Logged in as %s" % NICKNAME)
 
 def primsg(usr,msg):
     send_data('PRIVMSG %s %s' %(usr, msg))
-    
-def puzzle_ep1(txt):
-    match = re.search(r':([\d.]+) / ([\d.]+)',text)
-    #removed
-        primsg(BOT, puzzle+' -rep '+str(last))
 
-def puzzle_ep2(txt):
-    #removed
-      primsg(BOT, puzzle+' -rep '+str(bd))
-    
-def puzzle_ep3(txt):
-    #removed
-      primsg(BOT, puzzle+' -rep '+str(rt))
+def correct_response(msg):
+	match = re.search(r'You dit it', msg)
+	if match:
+		print('Good work. Try another?')
+		exit()
 
-def puzzle_ep4(txt):
-    #removed
-      primsg(BOT, puzzle+' -rep '+str(answer)+'\n')
+def bad_response(msg):
+	match = re.search(r'Bad reponse', msg)
+	if match:
+		print('Candy didn\'t like that.  Check your puzzlefu!')
+		exit()
         
+def solve_puzzle(ep,txt):
+    correct_response(txt)
+    bad_response(txt)
+    
+    if(ep) == '!ep1':
+        match = re.search(r'REMOVED',text)
+        if match:
+            #REMOVED
+            print('Sending: '+str(answer)+ ' to Candy.')
+            primsg(BOT, ep+' -rep '+str(answer)+'\n')
+            
+    if(ep) == '!ep2':
+        myregex =  'REMOVED'
+        match = re.search(myregex,text)
+        if match:
+            #REMOVED
+            print('Sending: '+str(answer)+ ' to Candy.')
+            primsg(BOT, ep+' -rep '+str(answer)+'\n')
+   
+    if(ep) == '!ep3':
+        myregex = 'REMOVED'
+        match = re.search(myregex,text)
+        if match:
+            #REMOVED
+            print('Sending: '+str(answer)+ ' to Candy.')
+            primsg(BOT, ep+' -rep '+str(answer)+'\n')
+     
+    if(ep == '!ep4'):
+        myregex = 'REMOVED'
+        match = re.search(myregex,text)
+        if match:
+            #REMOVED
+            print('Sending: '+str(answer)+ ' to Candy.')
+            primsg(BOT, ep+' -rep '+str(answer)+'\n')
+       
+
+#   Connect, login, join, and announce your question        
 irc_conn()
 login(NICKNAME)
 join(CHANNEL)
-time.sleep(3)
+time.sleep(2)
 primsg(BOT,puzzle)
 
+#   Solve the puzzle and get out of there
 while (1):
     text=IRC.recv(2040)
     text= str(text)
     if "PRIVMSG "+NICKNAME in text:
         print(text)
-        if puzzle == '!ep1':
-            puzzle_ep1(text)
-        elif puzzle == '!ep2':
-            puzzle_ep2(text)
-        elif puzzle == '!ep3':
-            puzzle_ep3(text)
-        elif puzzle == '!ep4':
-            puzzle_ep4(text)
+        solve_puzzle(puzzle,text)
             
         
